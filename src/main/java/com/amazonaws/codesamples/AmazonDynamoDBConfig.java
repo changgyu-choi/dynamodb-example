df06eefbbdc14e920fa9ff0a55763224e4ad8c3d
calvin.choi <calvin.choi@weversecompany.com>
2022-06-13 12:15:56 +0900

    Fix compile error

--- src/main/java/com/amazonaws/codesamples/AmazonDynamoDBConfig.java
+++ src/main/java/com/amazonaws/codesamples/AmazonDynamoDBConfig.java
@@ -1,10 +1,5 @@
 package com.amazonaws.codesamples;
 
-<<<<<<< Updated upstream
-import com.amazonaws.auth.AWSCredentialsProvider;
-import com.amazonaws.auth.profile.ProfileCredentialsProvider;
-=======
->>>>>>> Stashed changes
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
@@ -18,18 +13,6 @@ public class AmazonDynamoDBConfig {
     public AmazonDynamoDB amazonDynamoDB1() {
         return AmazonDynamoDBClientBuilder.standard()
                 .withRegion(Regions.AP_NORTHEAST_2)
-<<<<<<< Updated upstream
-                .withCredentials(baseCredentials)
-                .build();
-    }
-
-    @Profile("!default")
-    @Bean
-    public AmazonDynamoDB amazonDynamoDB3() {
-        return AmazonDynamoDBClientBuilder.standard()
-                .withRegion(Regions.AP_NORTHEAST_2)
-=======
->>>>>>> Stashed changes
                 .build();
     }
 }
